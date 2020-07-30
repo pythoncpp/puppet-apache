5e40f5cdce72bc986b04d1113b2d5b219fbc8b61
root <root@puppetserver.mylab.com>
2020-07-30 17:47:38 +0000

    added cutsom facts

--- spec/classes/service_spec.rb
+++ spec/classes/service_spec.rb
@@ -2,14 +2,11 @@
 
 require 'spec_helper'
 
-describe 'apache' do
+describe 'apache::service' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
 
-      #it { is_expected.to contain_class('apache::install') }
-      #it { is_expected.to contain_class('apache::service') }
-      #it { is_expected.to contain_class('apache::config') }
       it { is_expected.to compile }
     end
   end
